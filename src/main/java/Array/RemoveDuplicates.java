9d0efcf3c04b7d53c3fe1e8caa429a6bd722ee13
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-01 12:55:24 -0400

    refactored code to remove psvm

--- src/main/java/Array/RemoveDuplicates.java
+++ src/main/java/Array/RemoveDuplicates.java
@@ -7,13 +7,7 @@ package Array;
 
 public class RemoveDuplicates {
 
-    public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-
-        removeDuplicates(nums);
-    }
-
-    public static int removeDuplicates(int[] nums) {
+    public int removeDupes(int[] nums) {
         int index = 1;
 
         for(int i = 0; i < nums.length - 1; i++) {
