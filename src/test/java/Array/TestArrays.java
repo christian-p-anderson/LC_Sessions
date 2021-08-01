82dfb503a0657c6c04ffd9bf3ffe45697c9a2977
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-01 12:56:05 -0400

    tested RemoveDuplicates

--- src/test/java/Array/TestArrays.java
+++ src/test/java/Array/TestArrays.java
@@ -1,10 +1,20 @@
 package Array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestArrays {
 
     @Test
+    public void TestRemoveDuplicates() {
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int expectedResult = 5;
 
+        int result = removeDuplicates.removeDupes(nums);
+
+        Assert.assertEquals(expectedResult, result);
+
+    }
 
 }
