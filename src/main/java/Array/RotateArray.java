017ef2472e1a3625711d042b5b0e2509ae10c5fd
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-02 15:53:48 -0400

    completed RotateArray algo

--- src/main/java/Array/RotateArray.java
+++ src/main/java/Array/RotateArray.java
@@ -45,25 +45,50 @@ public class RotateArray {
 
 
 
-        k = k % nums.length;
-        int count = 0;
-        for (int start = 0; count < nums.length; start++) {
-            int current = start;
-            int prev = nums[start];
-            do {
-                int next = (current + k) % nums.length;
-                int temp = nums[next];
-                nums[next] = prev;
-                prev = temp;
-                current = next;
-                count++;
-            } while (start != current);
-        }
+//        k = k % nums.length;
+//        int count = 0;
+//        for (int start = 0; count < nums.length; start++) {
+//            int current = start;
+//            int prev = nums[start];
+//            do {
+//                int next = (current + k) % nums.length;
+//                int temp = nums[next];
+//                nums[next] = prev;
+//                prev = temp;
+//                current = next;
+//                count++;
+//            } while (start != current);
+//        }
 
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
     }
 
 }
 
 /*
-    The last block of code results in passing through the array only once resulting in a Big O notation of O(n)
+    This approach is based on the fact that when we rotate the array k times, k%nk elements from the back end of the
+    array come to the front and the rest of the elements from the front shift backwards.
+
+    In this approach, we firstly reverse all the elements of the array. Then, reversing the first k elements followed
+    by reversing the rest n-kn−k elements gives us the required result.
+
+    Let n = 7n=7 and k = 3k=3.
+
+    Original List                   : 1 2 3 4 5 6 7
+    After reversing all numbers     : 7 6 5 4 3 2 1
+    After reversing first k numbers : 5 6 7 4 3 2 1
+    After revering last n-k numbers : 5 6 7 1 2 3 4 --> Result
  */
