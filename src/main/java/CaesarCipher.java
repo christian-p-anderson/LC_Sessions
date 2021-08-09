e286b77e81b07ef940bc37b2964fe775e5d1babc
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-08 20:51:37 -0400

    added another algo CaesarCipher

---
+++ src/main/java/CaesarCipher.java
@@ -0,0 +1,22 @@
+public class CaesarCipher {
+
+    public static StringBuffer shiftEncrypt(String text, int shift) {
+        StringBuffer result = new StringBuffer();
+
+        /*
+            The key is the character value for A-Z is 65-90 and a-z is 97-122
+         */
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isUpperCase(text.charAt(i))) {
+                char character = (char)(((int)text.charAt(i) + shift - 65) % 26 + 65);
+                result.append(character);
+            } else {
+                char character = (char)(((int)text.charAt(i) + shift - 97) % 26 + 97);
+                result.append(character);
+            }
+        }
+
+        return result;
+    }
+}
