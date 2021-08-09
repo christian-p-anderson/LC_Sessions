f482cde47d9478633a9a1c003d127a7464133b2d
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-08 21:36:11 -0400

    added some notes

---
+++ src/main/java/Other/JP.java
@@ -0,0 +1,16 @@
+package Other;
+
+public class JP {
+
+    /*
+        JP Morgan is one of the largest global financial institutions, you help millions of people and businesses
+        JP Morgan is a technology company
+
+        I want to work for JP Morgan because I want to work for an organization where I will be continually challenged
+
+        It's important for me to continually grow and develop personally and professionally and this is a place where
+        I feel that I would be surrounded by like-minded people
+
+        I want to work somewhere that constantly innovates and improves
+     */
+}
