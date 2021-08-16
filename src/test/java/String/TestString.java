5f4f9ceeb0d28421ffe29e035bbf30afd6afc526
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-16 00:07:48 -0400

    created a Unit Test for ReverseAnInteger

--- src/test/java/String/TestString.java
+++ src/test/java/String/TestString.java
@@ -16,4 +16,16 @@ public class TestString {
 
         Assert.assertEquals(actual, result);
     }
+
+    @Test
+    public void TestReverseAnInteger() {
+        ReverseAnInteger reverseAnInteger = new ReverseAnInteger();
+
+        int intInput = 123;
+        int actual = 321;
+
+        int result = reverseAnInteger.reverse(intInput);
+
+        Assert.assertEquals(actual, result);
+    }
 }
