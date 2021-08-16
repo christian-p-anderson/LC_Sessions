cf85fe22d68032db7d5fc0053a9659a9caee138b
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-16 00:14:21 -0400

    provided context behind the algo for ReverseAnInteger

--- src/main/java/String/ReverseAnInteger.java
+++ src/main/java/String/ReverseAnInteger.java
@@ -1,3 +1,8 @@
+/*
+    Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside
+    the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+ */
+
 package String;
 
 public class ReverseAnInteger {
@@ -20,7 +25,7 @@ public class ReverseAnInteger {
             result = result * 10 + currentDigit;
 
             //this deals with the constraint, "If reversing x causes the value to go outside the signed 32-bit integer
-            //range [-231, 231 - 1], then return 0."
+            //range [-2^31, 2^31 - 1], then return 0."
             if ((result - currentDigit) / 10 != prevReversedNum) {
                 return 0;
             }
