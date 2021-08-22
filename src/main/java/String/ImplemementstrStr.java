87ab16b4ab1ac244314dc6e1483007d283a6f990
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-22 17:43:54 -0400

    completed algo for ImplmentstrStr

--- src/main/java/String/ImplemementstrStr.java
+++ src/main/java/String/ImplemementstrStr.java
@@ -21,15 +21,33 @@ public class ImplemementstrStr {
             return 0;
         }
 
-        int needleLength = needle.length();
-
-        //loop through the haystack and slide the window
-        for (int i = 0; i < haystack.length() - needleLength; i++) {
-            //check if the substring equals to the needle
-            if (haystack.substring(i, i + needleLength).equals(needle)) {
-                return i;
-            }
+        //special case
+        if(needle.length() == 0) {
+            return 0;
         }
+
+        for (int i = 0; i < haystack.length(); i++) {
+            //special case
+            if (i + needle.length() > haystack.length()) {
+                return -1;
+            }
+
+            int m = i;
+
+            for (int j = 0; j < needle.length(); j++) {
+                if(needle.charAt(j) == haystack.charAt(m)) {
+                    if (j == needle.length() - 1) {
+                        return i;
+                    }
+                    m++;
+                } else {
+                    break;
+                }
+            }
+
+        }
+
+
         return -1;
     }
 
