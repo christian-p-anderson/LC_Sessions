926e73872e704ffb653368aa315c6a3f00f5b7fc
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-24 19:18:39 -0400

    removed code that was commented out

--- src/main/java/Array/RotateArray.java
+++ src/main/java/Array/RotateArray.java
@@ -9,56 +9,6 @@ package Array;
 public class RotateArray {
 
     public int[] rotate(int[] nums, int k) {
-//        int j = 0;
-//
-//        while (k > 0) {
-//            int temp = arr[arr.length - 1];
-//
-//            for (j = arr.length - 1; j > 0; j--) {
-//                arr[j] = arr[j -1];
-//            }
-//
-//            arr[j] = temp;
-//            k--;
-//        }
-
-        // speed up the rotation
-//        k %= nums.length;
-//        int temp, previous;
-//        for (int i = 0; i < k; i++) {
-//            previous = nums[nums.length - 1];
-//            for (int j = 0; j < nums.length; j++) {
-//                temp = nums[j];
-//                nums[j] = previous;
-//                previous = temp;
-//            }
-//        }
-
-
-//        int[] a = new int[nums.length];
-//        for (int i = 0; i < nums.length; i++) {
-//            a[(i + k) % nums.length] = nums[i];
-//        }
-//        for (int i = 0; i < nums.length; i++) {
-//            nums[i] = a[i];
-//        }
-
-
-
-//        k = k % nums.length;
-//        int count = 0;
-//        for (int start = 0; count < nums.length; start++) {
-//            int current = start;
-//            int prev = nums[start];
-//            do {
-//                int next = (current + k) % nums.length;
-//                int temp = nums[next];
-//                nums[next] = prev;
-//                prev = temp;
-//                current = next;
-//                count++;
-//            } while (start != current);
-//        }
 
         k %= nums.length;
         reverse(nums, 0, nums.length - 1);
