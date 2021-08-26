2e497f3703b6b9c425ceae84a9ef9b78ccaf97b8
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-26 19:28:30 -0400

    created ListNode algo for use in ReverseLinkedList

---
+++ src/main/java/LinkedList/ListNodeOne.java
@@ -0,0 +1,26 @@
+/*
+    Associated with ReverseLinkedList
+
+    Definition for singly-linked list
+ */
+
+package LinkedList;
+
+public class ListNodeOne {
+    int val;
+
+    ListNodeOne next;
+
+    ListNodeOne() {
+    }
+
+    ListNodeOne(int val) {
+        this.val = val;
+    }
+
+    ListNodeOne(int val, ListNodeOne next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
