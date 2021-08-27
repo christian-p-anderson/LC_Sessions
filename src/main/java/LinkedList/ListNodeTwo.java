14f80fc775f42d4e932e1a9075bd2f6fa04fe3ae
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-27 17:33:52 -0400

    created ListNodeTwo for MergeTwoSortedLists

---
+++ src/main/java/LinkedList/ListNodeTwo.java
@@ -0,0 +1,15 @@
+/*
+    Associated with MergeTwoSortedLists
+
+    Definition for singly-linked list
+ */
+
+package LinkedList;
+
+public class ListNodeTwo {
+    int val;
+    ListNodeTwo next;
+    ListNodeTwo() {}
+    ListNodeTwo(int val) {this.val = val;}
+    ListNodeTwo(int val, ListNodeTwo next) {this. val = val; this.next = next;}
+}
