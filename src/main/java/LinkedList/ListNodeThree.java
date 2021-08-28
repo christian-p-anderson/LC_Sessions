d3dd9b284a30a98363ee94721362aaad15d9ff5e
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-28 16:18:27 -0400

    created ListNodeThree for PalindromeLinkedList

---
+++ src/main/java/LinkedList/ListNodeThree.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class ListNodeThree {
+    int val;
+    ListNodeThree next;
+
+    ListNodeThree() {}
+    ListNodeThree(int val) { this.val = val; }
+    ListNodeThree(int val, ListNodeThree next) { this.val = val; this.next = next; }
+}
