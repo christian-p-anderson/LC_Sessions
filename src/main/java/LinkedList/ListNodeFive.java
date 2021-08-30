e00cbffa9c29f1edbe2d23cd729259cec847dbb5
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-30 19:47:46 -0400

    created ListNodeFive for RemoveNodeFromEndOfList

---
+++ src/main/java/LinkedList/ListNodeFive.java
@@ -0,0 +1,16 @@
+/*
+    Associated to RemoveNodeFromEndOfList
+
+    Definition for singly-linked list
+ */
+
+package LinkedList;
+
+public class ListNodeFive {
+
+    int val;
+    ListNodeFive next;
+    ListNodeFive() {}
+    ListNodeFive(int val) { this.val = val; }
+    ListNodeFive(int val, ListNodeFive next) { this.val = val; this.next = next;}
+}
