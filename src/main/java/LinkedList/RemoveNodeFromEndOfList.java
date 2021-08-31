7c24068485ac7db15ffdbedf2455fb1d38cf3b31
Christian Anderson <christian.p.anderson1@gmail.com>
2021-08-30 21:40:38 -0400

    documented the code in RemoveNodeFromEndOfList

--- src/main/java/LinkedList/RemoveNodeFromEndOfList.java
+++ src/main/java/LinkedList/RemoveNodeFromEndOfList.java
@@ -17,32 +17,43 @@ package LinkedList;
 public class RemoveNodeFromEndOfList {
 
     public ListNodeFive removeNthFormEnd(ListNodeFive head, int n) {
-        ListNodeFive dummy_head = new ListNodeFive(0);
+        ListNodeFive dummy_head = new ListNodeFive(-1);
         dummy_head.next = head;
 
         ListNodeFive slow = dummy_head;
         ListNodeFive fast = dummy_head;
 
+        //this moves the fast pointer n spaces in front of the slow pointer
         for (int i = 1; i <= n + 1; i++) {
             fast = fast.next;
         }
 
+        //now move both pointers at the same time
+        //stop when the fast pointer equals null
         while (fast != null) {
             slow = slow.next;
             fast = fast.next;
         }
 
+        //point the slow pointer to the deleted node's successor (aka the node following the node that needs to be
+        //deleted)
         slow.next = slow.next.next;
 
+        //cuts off the dummy node
         return dummy_head.next;
 
     }
 }
 
 /*
-    use 2 pointers: fast and slow
-    traverse the fast pointer to position n + 1
-    traverse the slow pointer
+    use the 2 pointer technique with a fast and slow pointer
+    start with a dummy head
+
+    keep distance between the fast and slow pointer equal to n | move the fast node ahead of the slow node by n
+    then move both pointers at the same time
+
+    when the fast node reaches the end, have the slow node skip over the node that needs to be deleted
+
     what happens is that the node that needs to be deleted is no longer linked to it's predecessor
 
  */
