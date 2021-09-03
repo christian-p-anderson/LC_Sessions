3e3b4d55ddcbbdaf987a6bf7be49740c4aebc62e
Christian Anderson <christian.p.anderson1@gmail.com>
2021-09-03 19:02:22 -0400

    setup the SymmetricTree problem

---
+++ src/main/java/Trees/SymmetricTree.java
@@ -0,0 +1,31 @@
+/*
+    Given the root of a binary tree, check whether it is a mirror of itself (i.e. symmetric around its center)
+
+                   1
+                /     \
+               2       2
+             /  \     /  \
+            3    4   4    3
+
+    Input: root = [1, 2, 2, 3, 4, 4, 3]
+    Output: true
+
+                   1
+                /     \
+               2       2
+                \        \
+                 3        3
+
+    Input: root = [1, 2, 3, null, 3, null, 3]
+    Output: false
+ */
+
+package Trees;
+
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNodeTwo root) {
+
+        return false;
+    }
+}
