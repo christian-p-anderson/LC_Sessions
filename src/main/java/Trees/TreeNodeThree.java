5e70693d4b246ff7e5580a0d8501bfbdaf0d968b
Christian Anderson <christian.p.anderson1@gmail.com>
2021-09-04 21:42:33 -0400

    created TreeNodeThree for BinaryTreeOrderTraversal

---
+++ src/main/java/Trees/TreeNodeThree.java
@@ -0,0 +1,18 @@
+/*
+    Associated with BinaryTreeOrderTraversal
+ */
+
+package Trees;
+
+public class TreeNodeThree {
+    int val;
+    TreeNodeThree left;
+    TreeNodeThree right;
+    TreeNodeThree() {}
+    TreeNodeThree(int val) { this.val = val; }
+    TreeNodeThree(int val, TreeNodeThree left, TreeNodeThree right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
