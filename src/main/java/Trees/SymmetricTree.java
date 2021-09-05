4ab6766306a22b7e3bef057358cb28456a3c6550
Christian Anderson <christian.p.anderson1@gmail.com>
2021-09-05 15:59:39 -0400

    created algo for SymmetricTree

--- src/main/java/Trees/SymmetricTree.java
+++ src/main/java/Trees/SymmetricTree.java
@@ -26,6 +26,29 @@ public class SymmetricTree {
 
     public boolean isSymmetric(TreeNodeTwo root) {
 
-        return false;
+        //base case, if root double equals null
+        //we don't actually have a tree, but it would technically be symmetric
+        if (root == null) {
+            return true;
+        }
+
+        //this is the recursion, where we would compare the first set of left and right nodes
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNodeTwo left, TreeNodeTwo right) {
+        //base case
+        //if both left and right double equals null return the result of left double equals right
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        //if the left and right val don't equal each other return false
+        if (left.val != right.val) {
+            return false;
+        }
+
+        //this is where we walk down the binary tree and compare nodes
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
 }
